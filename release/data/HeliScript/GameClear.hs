df9c86c80466d563ae5d62b5e73f25bd01c77a55
Yuen-000 <85928662+Yuen-000@users.noreply.github.com>
2024-08-31 15:00:16 +0900

    GameOver

---
+++ release/data/HeliScript/GameClear.hs
@@ -0,0 +1,25 @@
+﻿component GameClear
+{
+    Item gameClearCream;
+
+    public GameClear()
+    {
+        gameClearCream = hsItemGet("GameClearCamera");
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void EnterClearArea(string clearArea)
+    {
+        IsGameClear();
+    }
+
+    public void IsGameClear()
+    {
+        
+    }
+
+}
---
+++ release/data/HeliScript/GameOver.hs
@@ -0,0 +1,60 @@
+component GameOver
+{
+    //自分GameoverScript
+    Item selfItem;
+
+    //デスポーンのアイテム
+    Item despawnHeightItem;
+
+    //カメラアイテム
+    Item camera; 
+
+    //ゲームオーバーかどうか
+    bool isGameOver; 
+
+    public GameOver()
+    {
+        selfItem = hsItemGetSelf();
+        despawnHeightItem = hsItemGet("RespownZone");
+        camera = hsItemGet("GameoverCamera");
+        isGameOver = false;
+    }
+
+    public void Update()
+    {
+        if(!isGameOver)
+        {
+            if(despawnHeightItem.GetPos().y >= hsPlayerGet().GetPos().y)
+            {
+                SetGameOver();
+            }
+        }
+    }
+
+    //ゲームオーバー画面に遷移する関数
+    void SetGameOver()
+    {
+        isGameOver = true;
+
+        //カメラをゲームオーバー画面のカメラに移す
+        camera.SetCamera();
+    }
+
+    //ボックスのどこかをクリックしたらリトライを行うための関数
+    public void OnClickNode(int nodeIndex)
+    {
+       SetRetry();
+    } 
+
+    //リトライ処理
+    void SetRetry()
+    {
+        isGameOver = false;
+
+        //カメラをプレイヤーに戻す
+        camera.ResetCamera(); 
+
+        //プレイヤーを初期位置に戻す
+        hsPlayerGet().SetPos(hsItemGet("PlayerSettings").GetPos());
+    }
+}
