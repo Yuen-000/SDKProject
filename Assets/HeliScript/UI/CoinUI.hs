4abce095044905dcbfda51d5f24266073003588f
Yuen-000 <85928662+Yuen-000@users.noreply.github.com>
2024-09-12 20:31:24 +0900

    UI scene

--- Assets/HeliScript/UI/CoinUI.hs
+++ Assets/HeliScript/UI/CoinUI.hs
@@ -1,12 +1,23 @@
 component CoinUI
 {
+    Item selfItem;
+    Player player;
+
     public CoinUI()
     {
-        
+        selfItem = hsItemGetSelf();
+        player = hsPlayerGet();
     }
 
     public void Update()
     {
-        
+        // Set the coin's position to match the player's position
+        Vector3 playerPos = player.GetPos();
+        hsItemSetPosition(selfItem, playerPos);
+    }
+
+    void hsItemSetPosition(Item self, Vector3 position)
+    {
+        item.SetPos(position);
     }
 }
