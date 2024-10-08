2fda158baf246f48fe82b4df3cc2c7ac637f9ae5
fsfujinity <huishitengzhi50@gmail.com>
2024-10-08 22:48:47 +0900

    新しいsceneを作成してassetを配置しました
    CreateStageCastleのsceneを作しました

--- release/data/Field/World/World_etc2.bat
+++ release/data/Field/World/World_etc2.bat
@@ -1,5 +1,10 @@
 md tex_etc\ 
 del tex_etc\*.pvr
-PVRTexToolCLI.exe -i "tex_sample\000_T_colorpallet_building_a.png" -o tex_etc\000.pvr -m 16 -f ETC2_RGB,UBN,sRGB
-PVRTexToolCLI.exe -i "tex_sample\001_CosmicCoolCloudFront.png" -o tex_etc\001.pvr -m 16 -f ETC2_RGB,UBN,sRGB
+PVRTexToolCLI.exe -i "tex_sample\000_Atlas_1.png" -o tex_etc\000.pvr -m 16 -f ETC2_RGBA,UBN,sRGB
+PVRTexToolCLI.exe -i "tex_sample\001_Atlas_1_Normal.png" -o tex_etc\001.pvr -m 16 -f ETC2_RGBA,UBN,sRGB
+PVRTexToolCLI.exe -i "tex_sample\002_Atlas_2.png" -o tex_etc\002.pvr -m 16 -f ETC2_RGBA,UBN,sRGB
+PVRTexToolCLI.exe -i "tex_sample\003_Atlas_2_Normal.png" -o tex_etc\003.pvr -m 16 -f ETC2_RGBA,UBN,sRGB
+PVRTexToolCLI.exe -i "tex_sample\004_Atlas_Alpha.png" -o tex_etc\004.pvr -m 16 -f ETC2_RGBA,UBN,sRGB
+PVRTexToolCLI.exe -i "tex_sample\005_Atlas_Alpha_Normal.png" -o tex_etc\005.pvr -m 16 -f ETC2_RGBA,UBN,sRGB
+PVRTexToolCLI.exe -i "tex_sample\006_Atlas_Alpha_Glow.png" -o tex_etc\006.pvr -m 16 -f ETC2_RGBA,UBN,sRGB
 pause
---
+++ release/data/HeliScript/HitBoxActionArea.hs
@@ -0,0 +1,17 @@
+component HitBoxActionArea
+{
+    public HitBoxActionArea()
+    {
+        hsSystemOutput("Hit Box Action Area\n");
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void PassingArea(string test)
+    {
+        hsSystemOutput("You have passed the area!\n");
+    }
+}
