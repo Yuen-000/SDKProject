9e308d6cb020e9f7e777a0d3acacecde527cd0f3
Yuen-000 <85928662+Yuen-000@users.noreply.github.com>
2025-01-11 18:47:19 +0900

    shieldsystem v01

--- Assets/HeliScript/Item/Shield/ShieldSystem.hs
+++ Assets/HeliScript/Item/Shield/ShieldSystem.hs
@@ -1,15 +1,49 @@
 component ShieldSystem
 {
-    Item shieldItem;
+    ItemDecision itemDecision;
 
+    Item shieldItem;
+    Player playerItem;
+
+    Vector3 playerPos;
+    Vector3 itemPos;
+
+    float judgmentDistance;
 
     public ShieldSystem()
     {
+        itemDecision = new itemDecision();
+
         shieldItem = hsItemGetSelf();
+        playerItem = new Player();
+
+        itemPos = shieldItem.GetPos();
+
+        shieldItem.SetShow(true);
+
+        judgmentDistance = shieldItem.GetProperty("Distance").ToFloat();
     }
 
     public void Update()
     {
-        
+        RangeDecision();
+    }
+
+    //アイテムとプレイヤーの距離判定
+    void RangeDecision()
+    {
+        playerPos = playerItem.GetPos();
+
+        if(playerPos.Distance(itemPos) < judgmentDistance)
+        {
+            itemDecision.SetItemUse(true);
+            shieldItem.SetShow(false);
+        }
+    }
+
+    //リセット
+    public void ResetShieldItem()
+    {
+        shieldItem.SetShow(ture);
     }
 }
