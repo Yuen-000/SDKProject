3a5dc0e8f8f260077201d7cb13673e0bfda0f0cc
Yuen-000 <85928662+Yuen-000@users.noreply.github.com>
2025-01-18 13:34:47 +0900

    delet release

--- release/data/Particle/Effect_Sheild_03/Effect_Sheild_03_astc.bat
+++
@@ -1,8 +0,0 @@
-md tex_astc\ 
-del tex_astc\*.pvr
-PVRTexToolCLI.exe -i "tex_sample\Texture000.png" -o tex_astc\Texture000.pvr -m 16 -f ASTC_4x4,UBN,sRGB
-PVRTexToolCLI.exe -i "tex_sample\Texture001.png" -o tex_astc\Texture001.pvr -m 16 -f ASTC_4x4,UBN,sRGB
-PVRTexToolCLI.exe -i "tex_sample\Texture002.png" -o tex_astc\Texture002.pvr -m 16 -f ASTC_4x4,UBN,sRGB
-PVRTexToolCLI.exe -i "tex_sample\Texture003.png" -o tex_astc\Texture003.pvr -m 16 -f ASTC_4x4,UBN,sRGB
-PVRTexToolCLI.exe -i "tex_sample\Texture004.png" -o tex_astc\Texture004.pvr -m 16 -f ASTC_4x4,UBN,sRGB
-pause
