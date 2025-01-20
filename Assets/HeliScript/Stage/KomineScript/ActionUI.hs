9693b24027659187904166599a70394a2fea0c72
koa0323 <100023641+koa0323@users.noreply.github.com>
2025-01-20 18:50:16 +0900

    0120連打アクション

--- Assets/HeliScript/Stage/KomineScript/ActionUI.hs
+++ Assets/HeliScript/Stage/KomineScript/ActionUI.hs
@@ -18,6 +18,9 @@ component ActionUI
     //Playerクラス
     Player  myPlayer;
 
+    //プレイヤーの座標
+    Vector3 playerPos;
+
     //自身
     Item myItemSelf;
 
@@ -57,12 +60,24 @@ component ActionUI
     //看板の枚数
     int SIGN_NUMMAX;
 
+    //矢印上のY座標
+    float ARROWU_Y;
+
+    //矢印左右のY座標
+    float ARROWLR_Y;
+
+    //矢印左のX座標;
+    float ARROWL_X;
+
+    //矢印右のX座標;
+    float ARROWR_X;
+
     public ActionUI()
     {
         hsSystemOutput("Script:ActionUI\n");
-        hsSystemOutput("Date:20241019\n");
-        hsSystemOutput("Version:2.0.2\n");
-        hsSystemOutput("Update Content:Remove button functions\n");
+        hsSystemOutput("Date:20250120\n");
+        hsSystemOutput("Version:3.0.1\n");
+        hsSystemOutput("Update Content:Support for New UI\n");
 
         myActionUIArrowU = hsItemGet("ActionUIArrowU");
         myActionUIArrowL = hsItemGet("ActionUIArrowL");
@@ -87,12 +102,54 @@ component ActionUI
         SIGN_LEFT_MOVELENGTH = myItemSelf.GetProperty("SIGN_LEFT_MOVELENGTH").ToFloat();
         SIGN_RIGHT_MOVELENGTH = myItemSelf.GetProperty("SIGN_RIGHT_MOVELENGTH").ToFloat();
         SIGN_NUMMAX = myItemSelf.GetProperty("SIGN_NUMMAX").ToInt();
+        ARROWU_Y = myItemSelf.GetProperty("ARROWU_Y").ToInt();
+        ARROWLR_Y = myItemSelf.GetProperty("ARROWLR_Y").ToInt();
+        ARROWL_X = myItemSelf.GetProperty("ARROWL_X").ToInt();
+        ARROWR_X = myItemSelf.GetProperty("ARROWR_X").ToInt();
     }
 
     public void Update()
     {
         if(isActionTime)
         {
+
+            if(actionFrame == 0)
+            {
+                myActionUIArrowU.SetShow(true);
+                myActionUIArrowL.SetShow(true);
+                myActionUIArrowR.SetShow(true);
+
+                for(int i = 0; i < SIGN_NUMMAX; i++)
+                {
+                    myActionUISign = hsItemGet("ActionUISign" + string(i));
+                    myActionUISign.SetShow(true);
+                }
+            }
+
+            playerPos = myPlayer.GetPos();
+
+            Vector3 UIPos = new Vector3();
+
+            //Up
+            UIPos.x = playerPos.x;
+            UIPos.y = playerPos.y + ARROWU_Y;
+            UIPos.z = playerPos.z;
+            myActionUIArrowU.SetPos(UIPos);
+
+
+            //Left
+            UIPos.x = playerPos.x + ARROWL_X;
+            UIPos.y = playerPos.y + ARROWLR_Y;
+            UIPos.z = playerPos.z;
+            myActionUIArrowL.SetPos(UIPos);
+
+            //Right
+            UIPos.x = playerPos.x + ARROWR_X;
+            UIPos.y = playerPos.y + ARROWLR_Y;
+            UIPos.z = playerPos.z;
+            myActionUIArrowR.SetPos(UIPos);
+
+
             for(int i = 0; i < SIGN_NUMMAX; i++)
             {
                 myActionUISign = hsItemGet("ActionUISign" + string(i));
@@ -161,8 +218,6 @@ component ActionUI
 
             pos.z = actionZ + float(num / 2) * SIGN_Z_INTERVAL + SIGN_Z_OFFSET;
 
-            hsSystemOutput(string(num) + "=" +string(pos.z));
-
             sign.SetPos(pos);
         }
     }
@@ -173,7 +228,6 @@ component ActionUI
             actionFrame = 0;
             hsSystemOutput("Button starts Action Time!\n");
             actionZ = actionZCoor.ToFloat();
-            hsSystemOutput(actionZCoor +  " & " + string(actionZ)+"\n");
         }
     }
 
@@ -181,6 +235,11 @@ component ActionUI
         if(isActionTime){
             isActionTime = false;
             actionFrame = 0;
+
+            myActionUIArrowU.SetShow(false);
+            myActionUIArrowL.SetShow(false);
+            myActionUIArrowR.SetShow(false);
+            
             for(int i = 0; i < SIGN_NUMMAX; i++)
             {
                 myActionUISign = hsItemGet("ActionUISign" + string(i));
